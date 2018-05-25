2446e4cfe22e49e80d7eef78f84b9209e630aea0
Stefan Prodan <stefan.prodan@gmail.com>
2018-05-25 12:00:53 +0300

    Change greetings

--- hallo/handler.go
+++ hallo/handler.go
@@ -6,5 +6,5 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello Stefan. You said: %s", string(req))
+	return fmt.Sprintf("Hello Alex. You said: %s", string(req))
 }
