b3e1f756236245b7e25dcf08d32804b23f8605c5
Alex Ellis <alexellis2@gmail.com>
2018-05-30 16:35:05 -0700

    Update handler.go

--- hallo/handler.go
+++ hallo/handler.go
@@ -6,5 +6,5 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("When 900 years old you reach, look as good you will not. %s", string(req))
+	return fmt.Sprintf("When 900 years old you reach, look as good you will not. %s (Yoda)", string(req))
 }
