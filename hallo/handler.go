7515d263f673d04eb88b7645e71a0938ae7f1f67
Alex Ellis <alexellis2@gmail.com>
2018-06-05 21:47:29 -0700

    Add secret sealed for K8s
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- hallo/handler.go
+++ hallo/handler.go
@@ -1,10 +1,17 @@
 package function
 
 import (
-	"fmt"
+	"io/ioutil"
 )
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("When 900 years old you reach, look as good you will not. %s (Yoda)", string(req))
+	file, err := ioutil.ReadFile("/var/openfaas/secrets/key")
+
+	if err != nil {
+		return err.Error()
+	}
+
+	return string(file)
+	// return fmt.Sprintf("When 900 years old you reach, look as good you will not. %s (Yoda)", string(req))
 }
