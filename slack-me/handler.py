f14afadfa267e2a31e282a84631e8300a20efcb5
Alex Ellis <alexellis2@gmail.com>
2018-06-13 11:26:34 -0700

    Remove emoticons

--- slack-me/handler.py
+++ slack-me/handler.py
@@ -11,11 +11,11 @@ def handle(req):
     if "user_name" in qs:
         if not qs["user_name"][0] == "slackbot":
             emoticons = ""
-            msg = qs["text"][0]
-            if "dockercon" in msg:
-                emoticons = ":whale:"
-            elif "serverless" in msg:
-                emoticons = ":openfaas: :robot_face:"
+#            msg = qs["text"][0]
+#            if "dockercon" in msg:
+#                emoticons = ":whale:"
+#            elif "serverless" in msg:
+#                emoticons = ":openfaas: :robot_face:"
             
             ret = { "text": qs["user_name"][0] + " sent a message with a length of: '" + str(len(req)) + "' " + emoticons }
             return json.dumps(ret)
