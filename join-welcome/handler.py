053bd5ed02963d607901ac5aeb8efbdc9b22f2e9
Alex Ellis <alexellis2@gmail.com>
2018-06-29 18:14:26 +0100

    Re-order file
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- join-welcome/handler.py
+++ join-welcome/handler.py
@@ -4,18 +4,13 @@ import random
 
 import requests
 
-def challenge(r):
-    if r["type"] == "url_verification":
-        res = {"challenge": r["challenge"]}
-        return json.dumps(res)
-
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
 
-    sys.stderr.write(req)
+    log_event(req)
 
     r = json.loads(req)
 
@@ -24,24 +19,46 @@ def handle(req):
 
     with open("/var/openfaas/secrets/incoming-webhook-url") as webhook_url_text:
         webhook_url = webhook_url_text.read().strip()
+
         if "event" in r:
-            if r["event"]["type"] == "member_joined_channel":
-                if "user" in r["event"]:
-                    user_name = r["event"]["user"]
-                    who = "<@" + user_name + ">"
-                    emoticon_list = [":openfaas:", ":whale:", ":thumbsup:", ":wave:", ":sunglasses:", ":ok_hand:", ":chart_with_upwards_trend:", ":sunrise:", ":smiley:", ":smiley_cat:", ":parrot:", ":rocket:", ":100:", ":muscle:", ":signal_strength:", ":man-cartwheeling:"]
+            target_channel = os.Getenv("target_channel")
+            return process_event(r, target_channel, webhook_url)
 
-                    emoticons = build_emoticons(emoticon_list)
+    return "Nothing to do with webhook"
 
-                    msg = {"text": "Let's all welcome {} to the community! {} ".format(who, emoticons.strip())}
+def challenge(r):
+    if r["type"] == "url_verification":
+        res = {"challenge": r["challenge"]}
+        return json.dumps(res)
 
-                    out_req = requests.post(webhook_url, json=msg)
-                    # print(str(out_req.status_code), out_req.text)
+def process_event(r, target_channel, webhook_url):
+    event_type = r["event"]["type"]
 
-def build_emoticons(emoticon_list):
-    emoticons = ""
-    sample = random.sample(emoticon_list, 5)
-    for em in sample:
-        emoticons = emoticons + em + " "
+    if r["event"]["channel"] == target_channel:
+        if event_type == "member_joined_channel":
+            if "user" in r["event"]:
+                user_name = r["event"]["user"]
+                who = "<@{}>".format(user_name)
 
-    return emoticons
+                positive_emoticons = [":openfaas:", ":whale:", ":thumbsup:", ":wave:", ":sunglasses:", ":ok_hand:", ":chart_with_upwards_trend:", ":sunrise:", ":smiley:", ":smiley_cat:", ":parrot:", ":rocket:", ":100:", ":muscle:", ":signal_strength:", ":man-cartwheeling:"]
+
+                emoticons = build_emoticons(positive_emoticons)
+
+                msg = {"text": "Let's all welcome {} to the community! {} ".format(who, emoticons.strip())}
+
+                out_req = requests.post(webhook_url, json=msg)
+                return ("{} response from Slack: {}".format(str(out_req.status_code), out_req.text))
+
+    return "Cannot process event_type: {}".format(event_type)
+
+def build_emoticons(emoticons):
+    emoticon_str = ""
+    sample = random.sample(emoticons, 5)
+
+    for emoticon in sample:
+        emoticon_str = emoticon_str + em + " "
+
+    return emoticon_str
+
+def log_event(req):
+    sys.stderr.write(req)
