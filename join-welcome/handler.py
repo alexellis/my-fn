1b92f8f4d6a070e686f1c7e959b7bc49038a5263
Alex Ellis <alexellis2@gmail.com>
2018-06-29 13:25:28 +0300

    Update join-welcome and strip away sample
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- join-welcome/handler.py
+++ join-welcome/handler.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import random
 
 import requests
 
@@ -23,8 +24,24 @@ def handle(req):
 
     with open("/var/openfaas/secrets/incoming-webhook-url") as webhook_url_text:
         webhook_url = webhook_url_text.read().strip()
+        if "event" in r:
+            if r["event"]["type"] == "member_joined_channel":
+                if "user" in r["event"]:
+                    user_name = r["event"]["user"]
+                    who = "<@" + user_name + ">"
+                    emoticon_list = [":openfaas:", ":whale:", ":thumbsup:", ":wave:", ":sunglasses:", ":ok_hand:", ":chart_with_upwards_trend:", ":sunrise:", ":smiley:", ":smiley_cat:", ":parrot:", ":rocket:", ":100:", ":muscle:", ":signal_strength:", ":man-cartwheeling:"]
 
-        msg = {"text": "Hello, World!"}
+                    emoticons = build_emoticons(emoticon_list)
 
-        # out_req = requests.post(webhook_url, json=msg)
-        #pr int(str(out_req.status_code), out_req.text)
+                    msg = {"text": "Let's all welcome {} to the community! {} ".format(who, emoticons.strip())}
+
+                    out_req = requests.post(webhook_url, json=msg)
+                    # print(str(out_req.status_code), out_req.text)
+
+def build_emoticons(emoticon_list):
+    emoticons = ""
+    sample = random.sample(emoticon_list, 5)
+    for em in sample:
+        emoticons = emoticons + em + " "
+
+    return emoticons
